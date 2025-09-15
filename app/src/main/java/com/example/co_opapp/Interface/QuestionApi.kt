ed6d5ac1ebd02ff3c2ab7520b295f062dc7d03b4
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-09-15 17:18:27 -0400

    c

---
+++ app/src/main/java/com/example/co_opapp/Interface/QuestionApi.kt
@@ -0,0 +1,17 @@
+package com.example.co_opapp.Interface
+
+import com.example.co_opapp.Service.AnswerResponse
+import com.example.co_opapp.data_model.AnswerRequest
+import com.example.co_opapp.data_model.TriviaQuestion
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface BackendQuestionApi {
+    @GET("api/game/questions/random")
+    suspend fun getRandomQuestion(): Response<TriviaQuestion>
+
+    @POST("api/game/questions/check-answer")
+    suspend fun checkAnswer(@Body answerRequest: AnswerRequest): Response<AnswerResponse>
+}
