9f6a341f042dc182bd52a041c9a4ac527f2e129e
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-09-15 00:38:52 -0400

    c

--- app/src/main/java/com/example/co_opapp/JwtResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.co_opapp
-
-data class JwtResponse(
-    val token: String,
-    val username:String
-)
--- app/src/main/java/com/example/co_opapp/UserCredentials.kt
+++
@@ -1,6 +0,0 @@
-package com.example.co_opapp
-
-data class UserCredentials(
-    val username: String,
-    val password: String
-)
