1e7732f85bdb98b037e3b4896034a3c7bc5971f1
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-09-17 03:11:13 -0400

    c

--- backend/src/main/java/com/example/spring_boot/Model/Question.java
+++ backend/src/main/java/com/example/spring_boot/Model/Question.java
@@ -1,6 +1,6 @@
 package com.example.spring_boot.Model;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
