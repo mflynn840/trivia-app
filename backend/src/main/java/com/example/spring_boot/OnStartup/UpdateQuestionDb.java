0a0a78f288b673d7ea61a62a2126aef278b02623
Michael Flynn <90886217+mflynn840@users.noreply.github.com>
2025-09-18 01:10:52 -0400

    c

--- backend/src/main/java/com/example/spring_boot/OnStartup/UpdateQuestionDb.java
+++ backend/src/main/java/com/example/spring_boot/OnStartup/UpdateQuestionDb.java
@@ -3,22 +3,19 @@ package com.example.spring_boot.OnStartup;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
 @Component
-public class DummyDataGenerator implements CommandLineRunner {
+public class UpdateQuestionDb implements CommandLineRunner {
     
     private final GenerateTriviaDb gen;
-    private final FetchTriviaQuestions fetch;
 
-    public DummyDataGenerator(GenerateTriviaDb gen, FetchTriviaQuestions fetch){
-        this.fetch = fetch;
+    public UpdateQuestionDb(GenerateTriviaDb gen, FetchTriviaQuestions fetch){
         this.gen = gen;
 
     }
     public void run(String... args) throws Exception {
-        gen.populate();
-        fetch.run();
+        gen.populate("./questions");
         
     }
-
 
 }
